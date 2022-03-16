511d9c5d92883ec158a4d3c76aeafbd9218fc1df
abhinay-developer <Sarala@736>
2022-03-16 09:38:44 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+ {path:'',component:HomeComponent},
+ {path:'home',component:HomeComponent},
+ {path:'login',loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)},
+ {path:'signup',loadChildren:()=>import('./modules/signup/signup.module').then(m=>m.SignupModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
